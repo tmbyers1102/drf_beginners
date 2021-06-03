75c0e3434f55d6e00860feee24e7827bacd08944
Tom Byers <tmbyers1102@gmail.com>
2021-06-03 14:57:55 -0700

    setting up drf stuff

---
+++ posts/serializers.py
@@ -0,0 +1,15 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'custom_id',
+            'category',
+            'publish_date',
+            'last_updated',
+        )
